3fe32cb455b68553e75f84ef387c18c1f9b4009c
MhmoudMoawad <moawadmhmoud@gmail.com>
2024-12-24 00:41:57 -0500

    start of code for KVL

--- kvl.py
+++ kvl.py
@@ -1,1 +1,7 @@
 #Program will use KVL to find the voltage of all componets in a circuit.
+
+
+print("Hello, welcome to the KVL calculator\n")
+print("How what is the source value of the circuit?: ")
+
+v = float(input("" ))
