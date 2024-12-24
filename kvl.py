cc945fd2028878145002c6e88c940ec8c6e3b72d
MhmoudMoawad <moawadmhmoud@gmail.com>
2024-12-24 16:03:27 -0500

    Update

--- kvl.py
+++ kvl.py
@@ -2,6 +2,12 @@
 
 
 print("Hello, welcome to the KVL calculator\n")
-print("How what is the source value of the circuit?: ")
+print("What type of circuit are you working with?: ")
+print("1.Series\n")
+print("2.Parrelel\n")
+print("3.Series Parelel\n")
+print("0.Exit\n")
 
-v = float(input("" ))
+
+source = float(input("How what is the source value of the circuit?: "))
+num_resistors = int(input("How many resistors?: "))
